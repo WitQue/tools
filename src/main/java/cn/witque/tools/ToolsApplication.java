4b13b239a9a439d5d4f0629a592b4acafd8aa34f
QUE <jiyankang@tanlunet.com>
2019-04-02 18:09:55 +0800

    Que
    First Commit

---
+++ src/main/java/cn/witque/tools/ToolsApplication.java
@@ -0,0 +1,19 @@
+package cn.witque.tools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableHystrix
+@EnableDiscoveryClient
+@SpringBootApplication
+@EnableFeignClients
+public class ToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToolsApplication.class, args);
+    }
+
+}
---
+++ src/main/java/cn/witque/tools/controller/ToolsController.java
@@ -0,0 +1,40 @@
+package cn.witque.tools.controller;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.UUID;
+
+@RestController
+@RefreshScope
+public class ToolsController {
+
+    @Value("${v2ray-demo-url} ")
+    private String url;
+
+    @GetMapping("/getV2rayConf")
+    public void export(HttpServletResponse response, String port) throws IOException {
+        String id = UUID.randomUUID().toString();
+        HttpClient client = new DefaultHttpClient();
+        HttpGet request = new HttpGet("http://a2hkc.witque.cn/demo.json");
+        HttpResponse resp = client.execute(request);
+        String demo = "";
+        if (resp.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+            demo = EntityUtils.toString(resp.getEntity());
+            demo = demo.replace("demoPort",""+port).replace("demoUuid", id);
+            byte[] bytes = demo.getBytes();
+            response.addHeader("Content-Type", "application/octet-stream");
+            response.addHeader("Content-Disposition", "attachment; filename=config.json");
+            response.getOutputStream().write(bytes);
+        }
+    }
+}
