6d264feec74ab02de5b11a3207dd0fb5b190db3f
QUE <jiyankang@tanlunet.com>
2019-04-10 17:02:01 +0800

    WitQue
    将demo配置放入项目中

--- src/main/java/cn/witque/tools/service/impl/V2rayServiceImpl.java
+++ src/main/java/cn/witque/tools/service/impl/V2rayServiceImpl.java
@@ -1,6 +1,7 @@
 package cn.witque.tools.service.impl;
 
 import cn.witque.tools.service.V2rayService;
+import org.apache.commons.io.FileUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.HttpClient;
@@ -8,9 +9,12 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.IOException;
 import java.util.UUID;
 
@@ -26,26 +30,22 @@ public class V2rayServiceImpl implements V2rayService {
      * @param response
      * @param port
      */
-    public void getV2rayConfig(HttpServletResponse response,String port){
+    public void getV2rayConfig(HttpServletResponse response, String port) {
         String id = UUID.randomUUID().toString();
-        HttpClient client = new DefaultHttpClient();
-        HttpGet request = new HttpGet(url);
-        HttpResponse resp = null;
+        String demo = null;
         try {
-        resp = client.execute(request);
-        String demo = "";
-        if (resp.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
-            demo = EntityUtils.toString(resp.getEntity());
-            demo = demo.replace("demoPort",""+port).replace("demoUuid", id);
+            ClassPathResource resource = new ClassPathResource("./static/demo.json");
+            File file = resource.getFile();
+            demo = FileUtils.readFileToString(file);
+            demo = demo.replace("65000", "" + port).replace("demoUuid", id);
             byte[] bytes = demo.getBytes();
             response.addHeader("Content-Type", "application/octet-stream");
             response.addHeader("Content-Disposition", "attachment; filename=config.json");
             response.getOutputStream().write(bytes);
+        } catch (IOException e1) {
+            e1.printStackTrace();
         }
-        }catch (IOException e) {
-            e.printStackTrace();
-        }
-
     }
+
 }
 
