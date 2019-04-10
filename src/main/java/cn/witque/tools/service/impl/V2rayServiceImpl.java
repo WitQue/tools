ef4d107bfbf8cf8319e33c13d792e397bc54f533
QUE <jiyankang@tanlunet.com>
2019-04-10 17:59:42 +0800

    WitQue
    Update 2019-04-10 17:59

--- src/main/java/cn/witque/tools/service/impl/V2rayServiceImpl.java
+++ src/main/java/cn/witque/tools/service/impl/V2rayServiceImpl.java
@@ -1,17 +1,19 @@
 package cn.witque.tools.service.impl;
 
 import cn.witque.tools.service.V2rayService;
-import org.apache.commons.io.FileUtils;
-import org.springframework.core.io.ClassPathResource;
+import cn.witque.tools.util.HttpClientUtil;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
 import java.io.IOException;
 import java.util.UUID;
 
 @Service
 public class V2rayServiceImpl implements V2rayService {
+
+    @Value("${v2ray-demo-url} ")
+    private String url;
 
     /**
      * 下载获取V2ray配置文件
@@ -20,11 +22,8 @@ public class V2rayServiceImpl implements V2rayService {
      */
     public void getV2rayConfig(HttpServletResponse response, String port) {
         String id = UUID.randomUUID().toString();
-        String demo = null;
         try {
-            ClassPathResource resource = new ClassPathResource("./static/demo.json");
-            File file = resource.getFile();
-            demo = FileUtils.readFileToString(file);
+            String demo = HttpClientUtil.doGet(url.replace(" ",""),null);
             demo = demo.replace("65000", "" + port).replace("demoUuid", id);
             byte[] bytes = demo.getBytes();
             response.addHeader("Content-Type", "application/octet-stream");
